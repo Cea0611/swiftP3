c5e7be4b446b21917917d5e3b4fae1578b5d9fea
Cesar Elizarraraz Acosta <cea73239@udelasalle.edu.mx>
2022-06-09 16:57:09 -0500

    Initial Commit

---
+++ ExamenP3/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  ExamenP3
+//
+//  Created by ISSC_412_2022 on 09/06/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
